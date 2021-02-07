04f56dcdc563c28f88984ec5a6efb2ac42e155a6
Sajid Chowdhury <msrchk@gmail.com>
2021-02-07 21:18:03 +0600

    last push

--- main.js
+++ main.js
@@ -8,19 +8,20 @@ const searchButtonClick = () => {
             const meals = data.meals;
             if (inputValue === "") {
                 alert("Please Enter your Food Name");
-                detailSectionHide("errorSection","none");
+                detailSectionHide("errorSection", "none");
+                detailSectionHide("details", "none")
             }
             else if (meals === null) {
-                 const errorSection = document.getElementById("errorSection");
-                 errorSection.style.display = 'block';
-                detailSectionHide("details","none");
+                const errorSection = document.getElementById("errorSection");
+                errorSection.style.display = 'block';
+                detailSectionHide("details", "none");
             }
             else {
                 meals.forEach(foodItem => {
                     const Id = foodItem.idMeal;
                     const newDiv = document.createElement("div");
-                    detailSectionHide("details","none");
-                    detailSectionHide("errorSection","none");                    
+                    detailSectionHide("details", "none");
+                    detailSectionHide("errorSection", "none");
                     const searchResult = `<a href="#details"><div class="foodItems" onclick="mealDetail(${Id})"><img src="${foodItem.strMealThumb}" > 
              <h2>${foodItem.strMeal}</h2></div></a>
              `;
@@ -61,12 +62,12 @@ const mealDetail = foodId => {
             })
         })
 }
-function detailSectionHide(id,value){
-    if(id==("details")){
-    document.getElementById(id).style.display = value;
-    }
-    if(id==("errorSection")){
+function detailSectionHide(id, value) {
+    if (id == ("details")) {
         document.getElementById(id).style.display = value;
-        }
+    }
+    if (id == ("errorSection")) {
+        document.getElementById(id).style.display = value;
+    }
 }
 
